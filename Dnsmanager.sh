4aba0bf58b8b63955f9c1925157b6f573834ec2b
Mohammadmehdihoseinii <mohammadmehdihossaniaslnejad@gmail.com>
2025-02-09 15:47:02 +0330

    Manage creation of files in the folder and add logs

--- Dnsmanager.sh
+++ Dnsmanager.sh
@@ -2,25 +2,58 @@
 
 nameApp="Dnsmanager"
 FileDnsName="ListDns.txt"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DIRFile="$DIR/$nameApp"
 declare -A List_Server=()
 # ---- function bash. ----
-# ---- strat:function file. ----
-function CreateFile () {
-    echo "-- list server Dns --" > ./ListDns.txt
-    echo "Google;8.8.8.8;8.8.4.4" >> ./ListDns.txt
-    echo "Cloudflare;1.1.1.1;1.0.0.1" >> ./ListDns.txt
-    echo "OpenDNS;185.55.226.26;185.55.226.25" >> ./ListDns.txt
-    echo "Neustar;156.154.70.5;156.154.71.5" >> ./ListDns.txt
-    echo "SafeDNS;195.46.39.39;195.46.39.40" >> ./ListDns.txt
-    echo "Shecan;178.22.122.100;185.51.200.2" >> ./ListDns.txt
-    echo "Electro;78.157.42.100;78.157.42.101" >> ./ListDns.txt
-    echo "RadarGame;10.202.10.10;10.202.10.11" >> ./ListDns.txt
-    echo "Online403;10.202.10.102;10.202.10.202" >> ./ListDns.txt
-    echo "Begzar;185.55.226.26;185.55.226.25" >> ./ListDns.txt
-    loadFile
+# ---- strat:Folder. ----
+function createdata(){   
+    function CreatetextDns () {
+        if [ -f "/$DIRFile/ListDns.txt" ]; then
+            echo "File found!"
+        else   
+            echo "#list-server-Dns" > /$DIRFile/ListDns.txt
+            echo "Google;8.8.8.8;8.8.4.4" >> /$DIRFile/ListDns.txt
+            echo "Cloudflare;1.1.1.1;1.0.0.1" >> /$DIRFile/ListDns.txt
+            echo "OpenDNS;185.55.226.26;185.55.226.25" >> /$DIRFile/ListDns.txt
+            echo "Neustar;156.154.70.5;156.154.71.5" >> /$DIRFile/ListDns.txt
+            echo "SafeDNS;195.46.39.39;195.46.39.40" >> /$DIRFile/ListDns.txt
+            echo "Shecan;178.22.122.100;185.51.200.2" >> /$DIRFile/ListDns.txt
+            echo "Electro;78.157.42.100;78.157.42.101" >> /$DIRFile/ListDns.txt
+            echo "RadarGame;10.202.10.10;10.202.10.11" >> /$DIRFile/ListDns.txt
+            echo "Online403;10.202.10.102;10.202.10.202" >> /$DIRFile/ListDns.txt
+            echo "Begzar;185.55.226.26;185.55.226.25" >> /$DIRFile/ListDns.txt
+            #append log
+            echo "Create db Dns " >> /$DIRFile/log.txt
+        fi
+        loadFile
+    }
+    function createtextlog() {
+        if [ -f "/$DIRFile/log.txt" ]; then
+            echo "File found!"
+        else
+            echo "app = '$nameApp v1'" > /$DIRFile/log.txt
+        fi
+    }
+    function createFolder() {
+        #! echo "$DIR"
+        if [ -d "/$DIRFile" ]; then
+            echo "Directory found"
+        else
+            mkdir -m 777 /$DIR/Dnsmanager
+        fi
+    }
+    createFolder
+    createtextlog
+    CreatetextDns
+    
+    clear
+
 }
+# ---- End:Folder. ----
+# ---- strat:function file. ----
 function loadFile () {
-    for FILE in `find . -iname "$FileDnsName" -type f`
+    for FILE in `find /$DIRFile -iname "$FileDnsName" -type f`
     do
         #Some function on the file
         #echo "FILE = $FILE"
@@ -29,6 +62,7 @@ function loadFile () {
                 IFS=';'
                 splitline=($fileline)
                 List_Server["${splitline[0]}"]="${splitline[1]};${splitline[2]}"
+                IFS=""
             fi
         done
     done
@@ -61,7 +95,7 @@ function Main () {
 				if [[ $Selectmenu =~ ^[0-9]+$ ]]; then
 					#echo "${Selectmenu} is a number"
 					if [ $Selectmenu == 1 ]; then
-						    echo "Select Dns"
+                        echo "Select Dns"
 					else
 						echo "Number over list"
 					fi
@@ -88,6 +122,7 @@ if [ $EUID != 0 ]; then
     exit $?
 else
     # Checking chmod file
+    createdata
     Main
 fi
 
